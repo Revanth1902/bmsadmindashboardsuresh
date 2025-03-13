ed0da7cedd4508c87cf0f7b2729c9cd3e036e2a0
Revanth1902 <revanth19a@gmail.com>
2025-03-13 11:01:32 +0530

    admin dashboard , users, movies ,theatres

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,3 +8,13 @@
 //   },
 //   plugins: [],
 // };
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Add the paths to all your source files where Tailwind classes are used
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
